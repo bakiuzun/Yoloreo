77be91d8cbb18d831614e0435242a12ef180ac20
bakiuzun <u.officialdeveloper@gmail.com>
2023-12-17 20:32:48 +0100

    evaluate file added

---
+++ evaluate.py
@@ -0,0 +1,32 @@
+import torch
+from validator import MyDetectionValidator
+from dataset import CliffDataset
+from torch.utils.data import DataLoader
+from model import MyYolo
+
+model_config = {
+    "arch": "yolov8.yaml",
+    "checkpoint":"weights_0/best.pt"
+}
+
+device  = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+## model def
+model = MyYolo(cfg=model_config["arch"])
+model.load_pretrained_weights(model_config["checkpoint"])
+model.nc = 1
+model.names = {0:'erosion'}
+
+
+for k, v in model.named_parameters():v.requires_grad = False
+
+
+model.to(device)
+
+
+validation_dataset = CliffDataset(mode="valid")
+
+validation_loader =  DataLoader(validation_dataset ,batch_size=32,shuffle=False)
+validator = MyDetectionValidator(dataloader=validation_loader,dataset=validation_dataset)
+
+validator.evaluate(model,device)
